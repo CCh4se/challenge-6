9b9f71a5df315fca178d39b2113b5a8437d8ab0a
Chase Miller <chasehmiller97@gmail.com>
2023-04-27 23:43:42 -0400

    final commit

---
+++ README.md
@@ -0,0 +1,22 @@
+## CHALLENGE -6
+
+## Weather Dashboard 
+
+# Screenshot
+
+
+
+# Description
+For this challenege we were tasked with creating a website that allows us to access Server Side APIs and implement them into the content we use. The code we emplemented needed us to be able to search for a city, view the current weather conditions for the city that was searched, view the future weather conditions, and to be able to click on the previous cities in the search history. Specically for the weather search, the user should be able to see the weather condition (via an icon), the temperature, the humidity, and the wind speed. I definetly to meet with a tutor this following week to brush up on all of this and get a better understanding of Server Side APIs.
+
+# Installation
+n/a
+
+# Usage
+n/a
+
+# Contributing 
+I follow up with a tutor to gain a better understanding with localStorage. Also used https://getbootstrap.com/, https://www.w3schools.com/, https://developer.mozilla.org/, and also https://openweathermap.org/. All of these websites really allowed me to implement all these tools into the website.
+
+# License
+n/a
--- script.js
+++ script.js
@@ -1,1 +1,78 @@
-var openWeatherUrl = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}'
+// var key = "2c9d51edd3cd7b196d7798f8d5722ef0";
+//
+//
+//
+//
+//
+//
+// function attached to the button to pull the city information from the field where the city was typed.
+function getWeather(){
+    var newCity = document.getElementById("citySearch");
+    var cityName = document.getElementById("cityName");
+    cityName.innerHTML ="--"+newCity.value+"--";
+}
+
+// variables for the 3 speperate server-side APIs
+var fiveDayForecast = "https://api.openweathermap.org/data/2.5/forecast?q=+newCity.value+&appid=2c9d51edd3cd7b196d7798f8d5722ef0";
+
+var weatherForecast = "https://api.openweathermap.org/data/2.5/forecast?lat=35&lon=80&appid=2c9d51edd3cd7b196d7798f8d5722ef0";
+
+var geoCoding = "http://api.openweathermap.org/geo/1.0/direct?q=+newCity.value+&limit=5&appid=2c9d51edd3cd7b196d7798f8d5722ef0";
+
+// fetch forecast
+fetch(fiveDayForecast)
+    .then(function(response) {
+        if (!response.ok) throw new Error('Huh?');
+ 
+        console.log('response :>>', response);
+
+        return response.json();
+})
+    .then(function(data) {
+        console.log('data :>>', data);
+
+        var dump = document.createElement('pre');
+        dump.textContent = JSON.stringify(data, null, 2);
+        document.body.appendChild(dump);
+})
+    .catch(function(error) {
+        console.log(error);
+});
+
+fetch(weatherForecast)
+    .then(function(response) {
+        if (!response.ok) throw new Error('Huh?');
+ 
+        console.log('response :>>', response);
+
+        return response.json();
+})
+    .then(function(data) {
+        console.log('data :>>', data);
+
+        var dump = document.createElement('pre');
+        dump.textContent = JSON.stringify(data, null, 2);
+        document.body.appendChild(dump);
+})
+    .catch(function(error) {
+        console.log(error);
+});
+
+fetch(geoCoding)
+    .then(function(response) {
+        if (!response.ok) throw new Error('Huh?');
+ 
+        console.log('response :>>', response);
+
+        return response.json();
+})
+    .then(function(data) {
+        console.log('data :>>', data);
+
+        var dump = document.createElement('pre');
+        dump.textContent = JSON.stringify(data, null, 2);
+        document.body.appendChild(dump);
+})
+    .catch(function(error) {
+        console.log(error);
+});
